34f15fae2821bb7b958e8b3d7396c9ba85cc8d70
Sumner Hearth <sumnernh@gmail.com>
2024-02-15 13:02:46 -0500

    Add more options and windings

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@ use clap::Parser;
 use env_logger::Builder;
 use gaugemc::{CudaBackend, CudaError, DualState, SiteIndex};
 use log::LevelFilter;
-use ndarray::{Array1, Array2, Array6, Axis};
+use ndarray::{s, Array1, Array2, Array3, Array6, ArrayView1, ArrayView2, ArrayView3, Axis};
 use ndarray_npy::NpzWriter;
 use rand::prelude::SliceRandom;
 use rand::Rng;
+use serde::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter};
 use std::fs::File;
 use std::io::Write;
 
 /// Simple program to greet a person
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Serialize, Deserialize)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long, default_value_t = 1)]
@@ -40,11 +41,19 @@ struct Args {
     log_every: usize,
     #[arg(long, default_value_t = Potential::villain)]
     potential_type: Potential,
-    #[arg(short, long, default_value_t = String::from("out.npz"))]
-    output: String,
+    #[arg(short, long, default_value_t = String::from("action.npz"))]
+    output_action: String,
+    #[arg(long, default_value_t = false)]
+    output_winding: bool,
+    #[arg(long, default_value_t = 0)]
+    write_output_every: usize,
+    #[arg(long, default_value = None)]
+    config_input: Option<String>,
+    #[arg(long, default_value = None)]
+    config_output: Option<String>,
 }
 
-#[derive(clap::ValueEnum, Clone, Default, Debug)]
+#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize, Deserialize)]
 enum Potential {
     #[default]
     villain,
@@ -64,7 +73,7 @@ enum StepAction {
     GlobalUpdate,
     ParallelTempering,
 }
-fn run(args: &Args) -> Result<(Array2<f32>, Array1<f32>), CudaError> {
+fn run(args: &Args) -> Result<(Array2<f32>, Option<Array3<i32>>, Array1<f32>), String> {
     let mut vns = Array2::zeros((args.replicas, 64));
 
     let ks = match args.replicas {
@@ -75,6 +84,7 @@ fn run(args: &Args) -> Result<(Array2<f32>, Array1<f32>), CudaError> {
         }
     };
     log::debug!("Running on ks: {:?}", ks);
+    let ks = Array1::from_vec(ks);
 
     ndarray::Zip::indexed(&mut vns).for_each(|(r, np), x| {
         *x = match args.potential_type {
@@ -107,7 +117,8 @@ fn run(args: &Args) -> Result<(Array2<f32>, Array1<f32>), CudaError> {
         None,
         None,
         None,
-    )?;
+    )
+    .map_err(|x| x.to_string())?;
 
     let mut rng = rand::thread_rng();
     let mut local_versus_global = (0..args.local_updates_per_step)
@@ -134,31 +145,60 @@ fn run(args: &Args) -> Result<(Array2<f32>, Array1<f32>), CudaError> {
             &perms_a,
             &perms_b,
             &mut rng,
-        )?;
+        )
+        .map_err(|x| x.to_string())?;
     }
     log::info!("Done!");
 
-    let mut output = Array2::zeros((args.num_samples, args.replicas));
-    output.axis_iter_mut(Axis(0)).enumerate().try_for_each(
-        |(sample_number, mut sample)| -> Result<(), CudaError> {
-            if sample_number % args.log_every == 0 {
-                log::info!("Sampling {}/{}", sample_number, args.num_samples);
-            }
-            steps(
-                args,
-                &mut local_versus_global,
-                &mut state,
-                &perms_a,
-                &perms_b,
-                &mut rng,
+    let mut action_output = Array2::zeros((args.num_samples, args.replicas));
+    let mut winding_output = if args.output_winding {
+        Some(Array3::zeros((args.num_samples, args.replicas, 6)))
+    } else {
+        None
+    };
+
+    for sample_number in 0..args.num_samples {
+        if sample_number % args.log_every == 0 {
+            log::info!("Sampling {}/{}", sample_number, args.num_samples);
+        }
+
+        steps(
+            args,
+            &mut local_versus_global,
+            &mut state,
+            &perms_a,
+            &perms_b,
+            &mut rng,
+        )
+        .map_err(|x| x.to_string())?;
+
+        let energies = state.get_action_per_replica().map_err(|x| x.to_string())?;
+        let mut sample = action_output.index_axis_mut(Axis(0), sample_number);
+        sample.iter_mut().zip(energies).for_each(|(x, y)| *x = y);
+
+        if let Some(winding_output) = winding_output.as_mut() {
+            let windings = state.get_winding_per_replica().map_err(|x| x.to_string())?;
+            let mut winding_sample = winding_output.index_axis_mut(Axis(0), sample_number);
+            winding_sample
+                .iter_mut()
+                .zip(windings)
+                .for_each(|(x, y)| *x = y);
+        }
+
+        if args.write_output_every > 0 && (sample_number + 1) % args.write_output_every == 0 {
+            log::debug!("Writing output to {}", args.output_action);
+            let output_subview = action_output.slice(s![..=sample_number, ..]);
+            write_output(
+                output_subview,
+                winding_output.as_ref().map(|x| x.view()),
+                ks.view(),
+                &args.output_action,
             )?;
-            let energies = state.get_action_per_replica()?;
-            sample.iter_mut().zip(energies).for_each(|(x, y)| *x = y);
-            Ok(())
-        },
-    )?;
+        }
+    }
+
     log::info!("Done!");
-    Ok((output, Array1::from_vec(ks)))
+    Ok((action_output, winding_output, ks))
 }
 
 fn steps<R: Rng>(
@@ -184,26 +224,50 @@ fn steps<R: Rng>(
     Ok(())
 }
 
-fn main() -> Result<(), String> {
-    Builder::new()
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "{} [{}] - {}",
-                Local::now().format("%Y-%m-%d %H:%M:%S"),
-                record.level(),
-                record.args()
-            )
-        })
-        .filter(None, LevelFilter::Info)
-        .init();
-
-    let args = Args::parse();
-    let (output, ks) = run(&args).map_err(|x| x.to_string())?;
-    let mut npz = NpzWriter::new_compressed(File::create(args.output).map_err(|x| x.to_string())?);
-    npz.add_array("energies", &output)
+fn write_output<Str: AsRef<str>>(
+    energies: ArrayView2<f32>,
+    windings: Option<ArrayView3<i32>>,
+    ks: ArrayView1<f32>,
+    filename: Str,
+) -> Result<(), String> {
+    let mut npz =
+        NpzWriter::new_compressed(File::create(filename.as_ref()).map_err(|x| x.to_string())?);
+    npz.add_array("energies", &energies)
         .map_err(|x| x.to_string())?;
+    if let Some(windings) = windings {
+        npz.add_array("windings", &windings)
+            .map_err(|x| x.to_string())?;
+    }
     npz.add_array("ks", &ks).map_err(|x| x.to_string())?;
     npz.finish().map_err(|x| x.to_string())?;
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    env_logger::init();
+
+    let mut args = Args::parse();
+
+    if let Some(config_input) = args.config_input.clone() {
+        log::debug!("Reading config file from {}", config_input);
+        let f = File::open(&config_input).map_err(|x| x.to_string())?;
+        args = serde_yaml::from_reader(f).map_err(|x| x.to_string())?;
+        args.config_input = Some(config_input);
+    } else if let Some(config_output) = &args.config_output {
+        log::debug!("Writing config file to {}", config_output);
+        let f = File::create(config_output).map_err(|x| x.to_string())?;
+        serde_yaml::to_writer(f, &args).map_err(|x| x.to_string())?;
+    }
+
+    log::debug!("Config: {:?}", args);
+
+    let (energies, winding, ks) = run(&args).map_err(|x| x.to_string())?;
+    write_output(
+        energies.view(),
+        winding.as_ref().map(|x| x.view()),
+        ks.view(),
+        &args.output_action,
+    )?;
+
     Ok(())
 }
