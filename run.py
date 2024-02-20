9afd428ac0f3556d8be0a0882a68f00aebc3afad
Sumner Hearth <sumnernh@gmail.com>
2024-02-20 12:20:26 -0500

    Update runner logs

--- run.py
+++ run.py
@@ -7,13 +7,14 @@ if __name__ == "__main__":
     klows = [0.75, 0.76, 0.77, 0.775]
     khighs = [0.85, 0.79, 0.78, 0.777]
     for l in Ls:
+        print(f"Running on L={l}")
         lbasedir = os.path.join(basedir, f"L={l}")
         os.makedirs(lbasedir, exist_ok=True)
         for (klow, khigh) in zip(klows, khighs):
             klowstr = str(klow).replace('.', '')
             khighstr = str(khigh).replace('.', '')
             filetemp = f"out-k{klowstr}-k{khighstr}"
-            print(filetemp)
+            print(f"\t{filetemp}")
 
             configfile = os.path.join(lbasedir, filetemp + ".yaml")
             outputfile = os.path.join(lbasedir, filetemp + ".npz")
