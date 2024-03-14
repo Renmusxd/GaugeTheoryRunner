08e29bdb923a841143a0e3ea100f52333a0648b8
Sumner Hearth <sumnernh@gmail.com>
2024-03-14 11:42:49 -0400

    Update build dirs

--- run_rec.sh
+++ run_rec.sh
@@ -12,7 +12,7 @@ module load python3/3.10.12
 
 OUTPUT_DIR=$1
 
-mkdir $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
 cd $OUTPUT_DIR || exit
 export WD=$OUTPUT_DIR
 
@@ -22,20 +22,26 @@ nvidia-smi --list-gpus
 nvidia-smi -L
 nvidia-smi -q
 
+OWNDIR="build/$JOB_ID.$SGE_TASK_ID"
+mkdir -p $OWNDIR
+cd $OWNDIR || exit
+
 if [ -d GaugeTheoryRunner ]; then
   echo "Updating Repo"
   cd GaugeTheoryRunner || exit
   git pull
+  # git checkout nodata
 else
   echo "Cloning Repo into directory"
+  # git clone -b nodata --single-branch --depth 1 git@github.com:Renmusxd/GaugeTheoryRunner.git
   git clone --depth 1 git@github.com:Renmusxd/GaugeTheoryRunner.git
   cd GaugeTheoryRunner || exit
 fi
 
-#cargo build -j 1
-#RUSTEXE="target/debug/gauge_mc_runner"
-cargo build --release -j 1
-RUSTEXE="target/release/gauge_mc_runner"
+cargo build --release -j ${NSLOTS:-1}
+cd ../..
+RUSTEXE="$OWNDIR/GaugeTheoryRunner/target/release/gauge_mc_runner"
+PYTHONEXE="$OWNDIR/GaugeTheoryRunner/run_rec.py"
 
 # Now run main thing
 echo "Running python code"
@@ -50,12 +56,24 @@ POTENTIAL=$2
 
 TASK_INDEX="$((SGE_TASK_ID-1))"
 echo "Running task index: $TASK_INDEX"
-$EXE run_rec.py --potential_type=$POTENTIAL \
-    --output_directory "../$POTENTIAL" \
-    --system_sizes "${@:3}" \
-    --disable_global_moves \
-    --executable $RUSTEXE \
-    --device_id 0 \
-    --task_id "$TASK_INDEX"
 
+echo "
+$EXE $PYTHONEXE --potential_type=$POTENTIAL \
+--output_directory \"$POTENTIAL\" \
+--system_sizes \"${@:3}\" \
+--disable_global_moves \
+--executable $RUSTEXE \
+--device_id 0 \
+--task_id \"$TASK_INDEX\"
+"
+
+$EXE $PYTHONEXE --potential_type=$POTENTIAL \
+--output_directory "$POTENTIAL" \
+--system_sizes "${@:3}" \
+--disable_global_moves \
+--executable $RUSTEXE \
+--device_id 0 \
+--task_id "$TASK_INDEX"
+
+cd "$OWNDIR/GaugeTheoryRunner"
 cargo clean
