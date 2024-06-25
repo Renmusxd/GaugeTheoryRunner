d102ad9d92b438cf8e6f1a429ffc4f28d3cf5aa2
Sumner Hearth <sumnernh@gmail.com>
2024-06-25 14:51:55 -0400

    Add more digits

--- scripts/run_several_multiloops_mephi.py
+++ scripts/run_several_multiloops_mephi.py
@@ -5,7 +5,24 @@ import sys
 import numpy
 
 
-def run_for_l_and_ks(potential, L, ks, zfill=3, num_samples=4096, nr=8, basedir=".", executable=None):
+def rename_files(zfill=7, basedir="."):
+    potentials = ["villain", "cosine", "binary", "power"]
+    for file in os.listdir(basedir):
+        filename = os.path.join(basedir, file)
+        if filename.endswith(".npz"):
+            arr = numpy.load(filename)
+            potential = potentials[arr["potential"][()]]
+            L = int(arr["L"][()])
+            k = arr["k"][()]
+            kstr = str(int(k * (10 ** zfill))).zfill(zfill + 1)
+            num_samples = arr["num_samples"][()]
+            new_filename = os.path.join(f"{basedir}", f"markov_{potential}_L{L}_k{kstr}_n{num_samples}_s16.npz")
+            if filename != new_filename:
+                print(f"Renaming {filename} to {new_filename}")
+                os.rename(filename, new_filename)
+
+
+def run_for_l_and_ks(potential, L, ks, zfill=5, num_samples=4096, nr=8, basedir=".", executable=None):
     if executable is None:
         executable = "cargo run --release --bin markov --"
     run_index = L * L * nr + 1
@@ -38,6 +55,9 @@ if __name__ == "__main__":
         executable = None
     os.makedirs(basedir, exist_ok=True)
 
+    # First check for existing and rename if needed
+    rename_files(basedir=basedir)
+
     cosine_ks = sorted(set(numpy.concatenate([
         numpy.linspace(0.5, 1.5, 10),
         numpy.linspace(0.9, 1.2, 10),
@@ -45,8 +65,6 @@ if __name__ == "__main__":
         numpy.linspace(1.010, 1.015, 10),
         numpy.linspace(1.0, 1.15, 10),
     ])))
-    run_for_l_and_ks("cosine", L, cosine_ks, basedir=basedir, executable=executable)
-
     villain_ks = sorted(set(numpy.concatenate([
         numpy.linspace(0.5, 1.5, 10),
         numpy.linspace(0.6, 0.9, 10),
@@ -54,8 +72,6 @@ if __name__ == "__main__":
         numpy.linspace(0.75, 0.8, 10),
         numpy.linspace(0.77, 0.78, 10),
     ])))
-    run_for_l_and_ks("villain", L, villain_ks, basedir=basedir, executable=executable)
-
     binary_ks = sorted(set(numpy.concatenate([
         numpy.linspace(0.5, 1.5, 10),
         numpy.linspace(0.6, 0.9, 10),
@@ -63,4 +79,9 @@ if __name__ == "__main__":
         numpy.linspace(0.75, 0.8, 10),
         numpy.linspace(0.76, 0.77, 10),
     ])))
+
+    run_for_l_and_ks("cosine", L, cosine_ks, basedir=basedir, executable=executable)
+
+    run_for_l_and_ks("villain", L, villain_ks, basedir=basedir, executable=executable)
+
     run_for_l_and_ks("binary", L, binary_ks, basedir=basedir, executable=executable)
