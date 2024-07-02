7b64cc46824a2face522edef95b63d3e8aaf3e77
Sumner Hearth <sumnernh@gmail.com>
2024-07-02 16:18:08 -0400

    Make into cosine

--- scripts/run_simple_sweep.sh
+++ scripts/run_simple_sweep.sh
@@ -12,4 +12,4 @@ module load cuda/12.2
 
 OUTPUT=$1
 
-target/release/gauge_mc_runner --klow 1.010 --khigh 1.015 --replicas-ks 16 --num-samples 1024 --warmup-samples 128 --systemsize 64 --steps-per-sample 16 --output $OUTPUT --global-updates-per-step 0
+RUST_LOG=info target/release/gauge_mc_runner --klow 1.010 --khigh 1.015 --replicas-ks 16 --num-samples 1024 --warmup-samples 128 --systemsize 64 --steps-per-sample 16 --output $OUTPUT --global-updates-per-step 0 --potential-type cosine
