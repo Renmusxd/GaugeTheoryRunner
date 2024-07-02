bd25349baf5c11233a95ad3a4060dfadd8f30de1
Sumner Hearth <sumnernh@gmail.com>
2024-07-02 12:02:12 -0400

    Silly script

---
+++ scripts/run_simple_sweep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#$ -N run_sweep
+#$ -cwd
+#$ -m ea
+#$ -M sumnerh@bu.edu
+#$ -l h_rt=24:00:00
+#$ -l gpus=1
+#$ -l gpu_c=6.0
+#$ -l gpu_memory=16G
+
+module load cuda/12.2
+
+OUTPUT=$1
+
+target/release/gauge_mc_runner --klow 1.010 --khigh 1.015 --replicas-ks 16 --num-samples 1024 --warmup-samples 128 --systemsize 64 --steps-per-sample 16 --output $OUTPUT --global-updates-per-step 0
