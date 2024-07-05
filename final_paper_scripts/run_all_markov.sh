5c1014ea064c421600dc57aa4e6bf82ca3ad746f
Sumner Hearth <sumnernh@gmail.com>
2024-07-05 17:44:14 -0400

    markov all script

--- final_paper_scripts/run_all_markov.sh
+++ final_paper_scripts/run_all_markov.sh
@@ -1,4 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash -l
+#$ -N markov_final
+#$ -cwd
+#$ -m ea
+#$ -M sumnerh@bu.edu
+#$ -l h_rt=72:00:00
+#$ -l gpus=1
+#$ -l gpu_c=6.0
+#$ -l gpu_memory=8G
+
+if [ "$SKIP_MODULE" != "true" ]; then
+  module load cuda/12.2
+  module load python3/3.10.12
+fi
+if [ -z "$PYTHONEXE" ]; then
+  PYTHONEXE="$HOME/.virtualenvs/gaugemc/bin/python"
+fi
 
 export RUST_LOG=info
 
@@ -8,17 +24,17 @@ Ls=( "4" "6" "8" "10" "12" "14" "16" )
 NUM_SAMPLES=4096
 
 # Express the K values in terms of integers so that our filenames aren't wonky.
-COMMON_KS=$( python -c "import numpy; ks = numpy.linspace(0.5,1.5,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
+COMMON_KS=$( $PYTHONEXE -c "import numpy; ks = numpy.linspace(0.5,1.5,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
 
-COSINE_BROAD_KS=$( python -c "import numpy; ks = numpy.linspace(0.9,1.1,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
-VILLAIN_BROAD_KS=$( python -c "import numpy; ks = numpy.linspace(1.2,1.4,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
-BINARY_BROAD_KS=$( python -c "import numpy; ks = numpy.linspace(1.2,1.4,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
+COSINE_BROAD_KS=$( $PYTHONEXE -c "import numpy; ks = numpy.linspace(0.9,1.1,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
+VILLAIN_BROAD_KS=$( $PYTHONEXE -c "import numpy; ks = numpy.linspace(1.2,1.4,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
+BINARY_BROAD_KS=$( $PYTHONEXE -c "import numpy; ks = numpy.linspace(1.2,1.4,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
 
-COSINE_FINE_KS=$( python -c "import numpy; ks = numpy.linspace(1.0,1.02,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
-VILLAIN_FINE_KS=$( python -c "import numpy; ks = numpy.linspace(1.279,1.299,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
-BINARY_FINE_KS=$( python -c "import numpy; ks = numpy.linspace(1.297,1.317,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
+COSINE_FINE_KS=$( $PYTHONEXE -c "import numpy; ks = numpy.linspace(1.0,1.02,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
+VILLAIN_FINE_KS=$( $PYTHONEXE -c "import numpy; ks = numpy.linspace(1.279,1.299,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
+BINARY_FINE_KS=$( $PYTHONEXE -c "import numpy; ks = numpy.linspace(1.297,1.317,11); mapf = lambda x: str(int(x*1e7)).zfill(8); kstrs = list(map(mapf, ks)); print(' '.join(kstrs))" )
 
-cargo build --release --bin markov
+cargo build --release --bin markov -j 1
 
 for L in ${Ls[*]}; do
   NRS=$(( L > 8 ? 8 : L ))
@@ -28,7 +44,7 @@ for L in ${Ls[*]}; do
     for KSTR in $COMMON_KS; do
       FILENAME="$OUTPUTDIR/markov/${POTENTIAL}/L=${L}/k=${KSTR}.npz"
       if ! [ -f $FILENAME ]; then
-        KFLOAT=$( python -c "print(int('${KSTR}')/1e7)" )
+        KFLOAT=$( $PYTHONEXE -c "print(int('${KSTR}')/1e7)" )
         target/release/markov --systemsize="${L}" \
         --output=$FILENAME \
         --k="$KFLOAT" \
@@ -45,7 +61,7 @@ for L in ${Ls[*]}; do
   for KSTR in $COSINE_BROAD_KS; do
     FILENAME="$OUTPUTDIR/markov/${POTENTIAL}/L=${L}/k=${KSTR}.npz"
     if ! [ -f $FILENAME ]; then
-      KFLOAT=$( python -c "print(int('${KSTR}')/1e7)" )
+      KFLOAT=$( $PYTHONEXE -c "print(int('${KSTR}')/1e7)" )
       target/release/markov --systemsize="${L}" \
       --output=$FILENAME \
       --k="$KFLOAT" \
@@ -60,7 +76,7 @@ for L in ${Ls[*]}; do
   for KSTR in $VILLAIN_BROAD_KS; do
     FILENAME="$OUTPUTDIR/markov/${POTENTIAL}/L=${L}/k=${KSTR}.npz"
     if ! [ -f $FILENAME ]; then
-      KFLOAT=$( python -c "print(int('${KSTR}')/1e7)" )
+      KFLOAT=$( $PYTHONEXE -c "print(int('${KSTR}')/1e7)" )
       target/release/markov --systemsize="${L}" \
       --output=$FILENAME \
       --k="$KFLOAT" \
@@ -75,7 +91,7 @@ for L in ${Ls[*]}; do
   for KSTR in $BINARY_BROAD_KS; do
     FILENAME="$OUTPUTDIR/markov/${POTENTIAL}/L=${L}/k=${KSTR}.npz"
     if ! [ -f $FILENAME ]; then
-      KFLOAT=$( python -c "print(int('${KSTR}')/1e7)" )
+      KFLOAT=$( $PYTHONEXE -c "print(int('${KSTR}')/1e7)" )
       target/release/markov --systemsize="${L}" \
       --output=$FILENAME \
       --k="$KFLOAT" \
@@ -91,7 +107,7 @@ for L in ${Ls[*]}; do
   for KSTR in $COSINE_FINE_KS; do
     FILENAME="$OUTPUTDIR/markov/${POTENTIAL}/L=${L}/k=${KSTR}.npz"
     if ! [ -f $FILENAME ]; then
-      KFLOAT=$( python -c "print(int('${KSTR}')/1e7)" )
+      KFLOAT=$( $PYTHONEXE -c "print(int('${KSTR}')/1e7)" )
       target/release/markov --systemsize="${L}" \
       --output=$FILENAME \
       --k="$KFLOAT" \
@@ -106,7 +122,7 @@ for L in ${Ls[*]}; do
   for KSTR in $VILLAIN_FINE_KS; do
     FILENAME="$OUTPUTDIR/markov/${POTENTIAL}/L=${L}/k=${KSTR}.npz"
     if ! [ -f $FILENAME ]; then
-      KFLOAT=$( python -c "print(int('${KSTR}')/1e7)" )
+      KFLOAT=$( $PYTHONEXE -c "print(int('${KSTR}')/1e7)" )
       target/release/markov --systemsize="${L}" \
       --output=$FILENAME \
       --k="$KFLOAT" \
@@ -121,7 +137,7 @@ for L in ${Ls[*]}; do
   for KSTR in $BINARY_FINE_KS; do
     FILENAME="$OUTPUTDIR/markov/${POTENTIAL}/L=${L}/k=${KSTR}.npz"
     if ! [ -f $FILENAME ]; then
-      KFLOAT=$( python -c "print(int('${KSTR}')/1e7)" )
+      KFLOAT=$( $PYTHONEXE -c "print(int('${KSTR}')/1e7)" )
       target/release/markov --systemsize="${L}" \
       --output=$FILENAME \
       --k="$KFLOAT" \
